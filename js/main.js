756eb5bdc361d0d8fd00f635e736a0a6dc78263a
luckypatcherx <59965177+luckypatcherx@users.noreply.github.com>
2022-08-02 18:14:17 +0530

    photo change
    photo changed

--- js/main.js
+++ js/main.js
@@ -388,5 +388,14 @@
             }
         });
     }
-
+    $(document).ready(function () {
+        //select the POPUP FRAME and show it
+        $("#popup").hide().fadeIn(3000);
+    
+        //close the POPUP if the button with id="close" is clicked
+        $("#close").on("click", function (e) {
+            e.preventDefault();
+            $("#popup").fadeOut(1000);
+        });
+    });
 })(jQuery);
